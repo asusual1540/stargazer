40ecd5b310bdf3174b739e5fba1fc67eec38d4f3
asusual1540 <mehedihasanadnan@gmail.com>
2020-01-21 03:03:51 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from "./static/images/logo.svg"
+import fb from "./static/images/facebook.svg"
+import twt from "./static/images/twitter.svg"
+import ytb from "./static/images/youtube.svg"
+import scld from "./static/images/soundcloud.svg"
+import vol from "./static/images/volume.svg"
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header"></div>
+      <div className="main-container">
+        <img src={logo} alt="logo" />
+        <div className="main-buttons">
+          <div className="main-button"><h4>EVENTS</h4></div>
+          <div className="main-button"><h4>GALLERY</h4></div>
+          <div className="main-button"><h4>CLIENTS</h4></div>
+          <div className="main-button"><h4>ABOUT</h4></div>
+          <div className="main-button"><h4>CONTACT</h4></div>
+        </div>
+      </div>
+      <div className="footer-container">
+        <div className="footer-first">
+          <h5>PRIVACY</h5>
+          <h5>COMMUNITY</h5>
+          <h5>HELP</h5>
+        </div>
+        <div className="footer-second">
+          <h5>FOLLOW US</h5>
+          <img src={fb} alt="facebook" />
+          <img src={twt} alt="twitter" />
+          <img src={ytb} alt="youtube" />
+          <img src={scld} alt="soundcloud" />
+        </div>
+        <div className="footer-third">
+          <div></div>
+          <div></div>
+          <div className="copyright-container">
+            <h5>ALL WORK COPYRIGHT OF STARGAZER <span>&copy;</span> 2020-2022 </h5>
+            <img className="volume" src={vol} alt="volume" />
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
