0c7e19a8949b6cdf9de088a95c4d6b3e3345115d
asusual1540 <mehedihasanadnan@gmail.com>
2020-01-21 20:59:18 +0600

    Updates

---
+++ precache-manifest.db7e1213a87f72cf14800b0a58b3ad5a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f59ac95a602fb926fca9af18dbbd9d7",
+    "url": "/stargazer/index.html"
+  },
+  {
+    "revision": "8abf40164faf61caa30b",
+    "url": "/stargazer/static/css/main.0a93dd1a.chunk.css"
+  },
+  {
+    "revision": "d8bf548092fa7e1d8f20",
+    "url": "/stargazer/static/js/2.9ba714a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stargazer/static/js/2.9ba714a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8abf40164faf61caa30b",
+    "url": "/stargazer/static/js/main.e3dedc29.chunk.js"
+  },
+  {
+    "revision": "572526b7eb693479d3a4",
+    "url": "/stargazer/static/js/runtime-main.c51e3698.js"
+  },
+  {
+    "revision": "b9a386f1e0c921d6a0f1d59d63f78afe",
+    "url": "/stargazer/static/media/background_1.b9a386f1.jpg"
+  },
+  {
+    "revision": "1db09742fbb597d6748e3d15b1472d8d",
+    "url": "/stargazer/static/media/facebook.1db09742.svg"
+  },
+  {
+    "revision": "d3565bc141cbd4bf6e924d568aa5c40c",
+    "url": "/stargazer/static/media/logo.d3565bc1.svg"
+  },
+  {
+    "revision": "c3bbb772002617f351745a8dc2aa1952",
+    "url": "/stargazer/static/media/soundcloud.c3bbb772.svg"
+  },
+  {
+    "revision": "7746198017a0b878fa3a713e12e04cde",
+    "url": "/stargazer/static/media/twitter.77461980.svg"
+  },
+  {
+    "revision": "6ac3d895cdd0f722eff1b62b68f8d51c",
+    "url": "/stargazer/static/media/volume.6ac3d895.svg"
+  },
+  {
+    "revision": "b916bca065a69cac7a5fe3d2c5fb52da",
+    "url": "/stargazer/static/media/youtube.b916bca0.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/stargazer/precache-manifest.db7e1213a87f72cf14800b0a58b3ad5a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stargazer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
