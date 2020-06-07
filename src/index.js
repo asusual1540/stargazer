ec41e3cf27155b7df5b7c03a41c77075ba5ee7b7
asusual1540 <mehedihasanadnan@gmail.com>
2020-06-08 02:27:01 +0600

    fixed routing attempt 5

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from "react-router-dom"
+import history from "./history"
 import './index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-    <Router history={BrowserRouter}>
+    <BrowserRouter history={history}>
         <App />
-    </Router>,
+    </BrowserRouter>,
     document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
